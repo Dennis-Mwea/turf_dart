f1bac230689eee332bda1e4483e198ca8ae92bb0
Dennis Mwea <mweadennis2@gmail.com>
2022-04-11 20:21:49 +0300

    Finish converting required code

--- lib/src/kinks.dart
+++ lib/src/kinks.dart
@@ -1,56 +1,59 @@
+import 'package:sweepline_intersections/sweepline_intersections.dart';
+import 'package:turf/helpers.dart';
+
+/// Takes any [LineString]/[Polygon] GeoJSON and returns the intersecting point(s).
 lineIntersects(
-  dynamic line1StartX,
-  dynamic line1StartY,
-  dynamic line1EndX,
-  dynamic line1EndY,
-  dynamic line2StartX,
-  dynamic line2StartY,
-  dynamic line2EndX,
-  dynamic line2EndY,
-) {
-  // if the lines intersect, the result contains the x and y of the
-  // intersection (treating the lines as infinite) and booleans for whether
-  // line segment 1 or line segment 2 contain the point
-  var denominator;
-  var a;
-  var b;
-  var numerator1;
-  var numerator2;
-  var result = {
-    'x': null,
-    'y': null,
-    'onLine1': false,
-    'onLine2': false,
-  };
+  dynamic line1,
+  dynamic line2, {
+  bool removeDuplicates = true,
+  bool ignoreSelfIntersections = false,
+}) {
+  List<Feature<GeometryObject>> features = [];
 
-  denominator = (line2EndY - line2StartY) * (line1EndX - line1StartX) - (line2EndX - line2StartX) * (line1EndY - line1StartY);
-  if (denominator == 0) {
-    return result['x'] != null && result['y'] != null ? result : false;
-  }
-  a = line1StartY - line2StartY;
-  b = line1StartX - line2StartX;
-  numerator1 = (line2EndX - line2StartX) * a - (line2EndY - line2StartY) * b;
-  numerator2 = (line1EndX - line1StartX) * a - (line1EndY - line1StartY) * b;
-  a = numerator1 / denominator;
-  b = numerator2 / denominator;
-
-  // if we cast these lines infinitely in both directions, they intersect here:
-  result['x'] = line1StartX + a * (line1EndX - line1StartX);
-  result['y'] = line1StartY + a * (line1EndY - line1StartY);
-
-  // if line1 is a segment and line2 is infinite, they intersect if:
-  if (a >= 0 && a <= 1) {
-    result['onLine1'] = true;
+  switch (line1.type.runtimeType) {
+    case FeatureCollection:
+      features.addAll((line1 as FeatureCollection).features);
+      break;
+    case Feature:
+      features.add(line1 as Feature);
+      break;
+    case LineString:
+    case Polygon:
+    case MultiLineString:
+    case MultiPolygon:
+      features.add(Feature(geometry: line1));
+      break;
   }
 
-  // if line2 is a segment and line1 is infinite, they intersect if:
-  if (b >= 0 && b <= 1) {
-    result['onLine2'] = true;
+  switch (line2.type.runtimeType) {
+    case FeatureCollection:
+      features.addAll((line2 as FeatureCollection).features);
+      break;
+    case Feature:
+      features.add(line2 as Feature);
+      break;
+    case LineString:
+    case Polygon:
+    case MultiLineString:
+    case MultiPolygon:
+      features.add(Feature(geometry: line2));
+      break;
   }
-  // if line1 and line2 are segments, they intersect if both of the above are true
-  if (result['onLine1'] as bool && result['onLine2'] as bool) {
-    return [result['x'], result['y']];
+
+  final intersections = sweeplineIntersections(FeatureCollection(features: features), ignoreSelfIntersections: ignoreSelfIntersections);
+  List<Position> results = [];
+  if (removeDuplicates) {
+    const Map<String, bool> unique = {};
+    intersections.forEach((intersection) {
+      final key = intersection.join(",");
+      if (!unique.containsKey(key)) {
+        unique[key] = true;
+        results.add(intersection);
+      }
+    });
   } else {
-    return false;
+    results = intersections;
   }
+
+  return FeatureCollection(features: results.map((e) => Feature(geometry: Point(coordinates: e))).toList());
 }
--- lib/src/pointOnLine.dart
+++ lib/src/pointOnLine.dart
@@ -8,39 +8,7 @@ import 'package:turf/meta.dart';
 import 'package:turf/src/invariant.dart';
 import 'package:turf/src/kinks.dart';
 
-abstract class NearestPointOnLine extends Feature<Point> {
-  Map<String, dynamic>? properties = {
-    // 'index'?: number;
-    // 'dist'?: number;
-    // 'location'?: number;
-    // [key: string]: any;
-  };
-}
-
-/// Takes a {@link Point} and a {@link LineString} and calculates the closest Point on the (Multi)LineString.
-///
-/// @name nearestPointOnLine
-/// @param {Geometry|Feature<LineString|MultiLineString>} lines lines to snap to
-/// @param {Geometry|Feature<Point>|number[]} pt point to snap from
-/// @param {Object} [options={}] Optional parameters
-/// @param {string} [options.units='kilometers'] can be degrees, radians, miles, or kilometers
-/// @returns {Feature<Point>} closest point on the `line` to `point`. The properties object will contain three values: `index`: closest point was found on nth line part, `dist`: distance between pt and the closest point, `location`: distance along the line between start and the closest point.
-/// @example
-/// var line = turf.lineString([
-///     [-77.031669, 38.878605],
-///     [-77.029609, 38.881946],
-///     [-77.020339, 38.884084],
-///     [-77.025661, 38.885821],
-///     [-77.021884, 38.889563],
-///     [-77.019824, 38.892368]
-/// ]);
-/// var pt = turf.point([-77.037076, 38.884017]);
-///
-/// var snapped = turf.nearestPointOnLine(line, pt, {units: 'miles'});
-///
-/// //addToMap
-/// var addToMap = [line, pt, snapped];
-/// snapped.properties['marker-color'] = '#00f';
+/// Takes a [Point] and a [LineString] and calculates the closest [Point] on the [LineString]/[MultiLineString].
 Feature<Point> nearestPointOnLine<G extends GeometryType>(Feature<G> lines, Point pt, {Map<String, dynamic>? options}) {
   var closestPt = Feature(
     geometry: Point(coordinates: Position(double.infinity, double.infinity, double.infinity)),
@@ -67,7 +35,7 @@ Feature<Point> nearestPointOnLine<G extends GeometryType>(Feature<G> lines, Poin
       final perpendicularPt2 = destination(pt, heightDistance, direction - 90);
       final intersect = lineIntersects(
         Feature(geometry: LineString(coordinates: [perpendicularPt1.coordinates, perpendicularPt2.coordinates])),
-        Feature(geometry: LineString(coordinates: [start.geometry.coordinates, stop.geometry.coordinates])),
+        Feature(geometry: LineString(coordinates: [start.geometry!.coordinates, stop.geometry!.coordinates])),
       );
       var intersectPt;
       if (intersect.features.length > 0) {
@@ -81,14 +49,14 @@ Feature<Point> nearestPointOnLine<G extends GeometryType>(Feature<G> lines, Poin
         closestPt.properties!['index'] = i;
         closestPt.properties!['location'] = length;
       }
-      if (stop.properties!['dist'] < closestPt.properties['dist']) {
+      if (stop.properties!['dist'] < closestPt.properties!['dist']) {
         closestPt = stop;
         closestPt.properties!['index'] = i + 1;
         closestPt.properties!['location'] = length + sectionLength;
       }
       if (intersectPt && intersectPt.properties!.dist < closestPt.properties!['dist']) {
         closestPt = intersectPt;
-        closestPt.properties['index'] = i;
+        closestPt.properties!['index'] = i;
       }
       // update length
       length += sectionLength;
